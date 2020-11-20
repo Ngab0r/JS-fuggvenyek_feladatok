927d0ba9e57fa37137568f70113c289377ccedfa
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2020-11-20 11:07:23 +0100

    feladat 2 pipa

---
+++ js/feladat2.js
@@ -0,0 +1,8 @@
+const summation = (a = 0, b = 0) => a + b;
+const subtration = (a = 0, b = 0) => a - b;
+
+console.log(summation(1, 2));
+console.log(summation(1));
+
+console.log(subtration(2, 1));
+console.log(subtration(2));
