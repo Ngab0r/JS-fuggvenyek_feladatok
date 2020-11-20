f9971676b84ffe912a08088eab3679588c66becf
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2020-11-20 10:52:39 +0100

    feladat 1 pipa

--- js/feladat1.js
+++ js/feladat1.js
@@ -1,5 +1,9 @@
 const buttons = document.querySelectorAll('.button');
 
 const handleClick = () => {
-
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', () => { console.log(buttons[i].textContent); });
+    }
 }
+
+handleClick();
